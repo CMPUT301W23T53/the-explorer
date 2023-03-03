54349e1ef69c14f863dbe31a2c1ff7918d41c9cf
Andrew Rosario <71676291+andrew-rosario@users.noreply.github.com>
2023-03-03 14:55:50 -0700

    Change Map button icon and title

--- app/src/main/java/com/example/theexplorer/ui/map/MapViewModel.java
+++ app/src/main/java/com/example/theexplorer/ui/map/MapViewModel.java
@@ -1,16 +1,16 @@
-package com.example.theexplorer.ui.notifications;
+package com.example.theexplorer.ui.map;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class MapViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public MapViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is the Maps fragment");
     }
 
     public LiveData<String> getText() {
