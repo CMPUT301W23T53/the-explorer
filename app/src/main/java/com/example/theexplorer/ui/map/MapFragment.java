7f8e40f095f5654d49814c4958c3db7839f6b22c
Shilong Luo <56777596+Er1c-luo@users.noreply.github.com>
2023-03-12 14:58:41 -0600

    Add map method
    Add the map method to mapfragment and add the permission into mainfest

--- app/src/main/java/com/example/theexplorer/ui/map/MapFragment.java
+++ app/src/main/java/com/example/theexplorer/ui/map/MapFragment.java
@@ -1,20 +1,43 @@
 package com.example.theexplorer.ui.map;
 
+import static android.content.Context.LOCATION_SERVICE;
+
+import android.annotation.SuppressLint;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.theexplorer.databinding.FragmentMapBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFragment extends Fragment {
+import java.util.Locale;
+
+public class MapFragment extends Fragment implements LocationListener {
 
     private FragmentMapBinding binding;
+    private GoogleMap mMap;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationManager locationManager;
+
+    View mView;
+    MapView mMapView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -22,10 +45,41 @@ public class MapFragment extends Fragment {
 
         binding = FragmentMapBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        final TextView textView = binding.textMap;
-        mapViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        mMapView= binding.map;
+        getCurrentLocation();
         return root;
+    }
+    @SuppressLint("MissingPermission")
+    private void getCurrentLocation() {
+        try {
+            locationManager = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,10,5,this);
+        }catch (Exception e){
+            e.printStackTrace();
+        }}
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        Toast.makeText(getActivity(), ""+location.getLatitude()+","+location.getLongitude(), Toast.LENGTH_SHORT).show();
+        try {
+            Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+            double currentLat = location.getLatitude();
+            double currentLong = location.getLongitude();
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(@NonNull GoogleMap googleMap) {
+                    mMap = googleMap;
+                    LatLng sydney = new LatLng(currentLat, currentLong);
+                    mMap.addMarker(new MarkerOptions().position(sydney).title("Current Location"));
+
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney,100));
+                }
+            });
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
