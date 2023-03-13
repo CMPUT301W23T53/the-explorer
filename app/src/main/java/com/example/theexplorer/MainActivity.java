43a41bd4eb68ed7c8769d960632df30065b3124a
Andrew Rosario <71676291+andrew-rosario@users.noreply.github.com>
2023-03-12 20:12:04 -0600

    Add / Override Navigation and Toolbar behaviour

--- app/src/main/java/com/example/theexplorer/MainActivity.java
+++ app/src/main/java/com/example/theexplorer/MainActivity.java
@@ -1,23 +1,32 @@
 package com.example.theexplorer;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.example.theexplorer.ui.home.HomeFragment;
+import com.example.theexplorer.ui.map.MapFragment;
 import com.example.theexplorer.ui.scan.ZXingScannerScan;
 import com.example.theexplorer.services.UserService;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.theexplorer.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,12 +53,63 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-        if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-        {
-            ActivityCompat.requestPermissions(MainActivity.this,new String[]{
+        if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION
-            },100);
+            }, 100);
         }
+
+        navView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment navHost = getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
+                int currentFragment = navHost.getChildFragmentManager().getFragments().get(0).getId();
+
+                if (item.getItemId() == R.id.navigation_home) {
+                    switchFragment(R.id.navigation_home);
+                    return true;
+                }
+                else if (item.getItemId() == R.id.navigation_scan)
+                {
+                    Intent toGo = new Intent(MainActivity.this, ZXingScannerScan.class);
+                    startActivity(toGo);
+                    return true;}
+                else if (item.getItemId() == R.id.navigation_map) {
+                    switchFragment(R.id.navigation_map);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Fragment navHost = getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
+        int currentFragment = navHost.getChildFragmentManager().getFragments().get(0).getId();
+
+        if (item.getItemId() == R.id.toolbar_profile) {
+            switchFragment(R.id.toolbar_profile);
+        }
+        else if (item.getItemId() == R.id.toolbar_search) {
+            Toast.makeText(this, "This button works", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void switchFragment(int fragmentID) {
+        Fragment navHost = getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
+        int currentFragment = navHost.getChildFragmentManager().getFragments().get(0).getId();
+        Fragment toSwitch = new Fragment(fragmentID);
+        navHost.getChildFragmentManager().beginTransaction().replace(currentFragment,toSwitch).commit();
     }
 
 }
