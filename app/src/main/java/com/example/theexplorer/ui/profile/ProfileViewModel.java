76d8d5a6b1ad1aa54a1376f8403f876e656c66c6
Andrew Rosario <71676291+andrew-rosario@users.noreply.github.com>
2023-03-12 20:56:08 -0600

    Modify / Implement ProfileViewModel for testing purposes

--- app/src/main/java/com/example/theexplorer/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/example/theexplorer/ui/profile/ProfileViewModel.java
@@ -1,7 +1,18 @@
 package com.example.theexplorer.ui.profile;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class ProfileViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private final MutableLiveData<String> mText;
+
+    public ProfileViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is the Profile fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
 }
