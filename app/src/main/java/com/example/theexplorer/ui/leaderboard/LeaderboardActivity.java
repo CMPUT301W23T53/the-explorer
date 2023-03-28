b3b3a94e25f070b49cd3c50fd81ce9b8cd5443ce
Andrew Rosario <71676291+andrew-rosario@users.noreply.github.com>
2023-03-27 18:04:16 -0600

    Add / Completed LeaderboardActivity

--- app/src/main/java/com/example/theexplorer/ui/leaderboard/LeaderboardActivity.java
+++ app/src/main/java/com/example/theexplorer/ui/leaderboard/LeaderboardActivity.java
@@ -2,12 +2,33 @@ package com.example.theexplorer.ui.leaderboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 import com.example.theexplorer.R;
+import com.example.theexplorer.services.User;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LeaderboardActivity extends AppCompatActivity {
+
+    private Leaderboard leaderboard;
+    private ArrayList<User> usersDataList;
+    private LeaderboardAdapter usersAdapter;
+
+    private Spinner userScope;
+    private Spinner listFilter;
+    private ArrayAdapter<String> userScopeAdapter;
+    private ArrayAdapter<String> listFilterAdapter;
+    private ListView usersList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +36,7 @@ public class LeaderboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_leaderboard);
         Toolbar toolbar = findViewById(R.id.leaderboard_toolbar);
 
-        String userName = String.valueOf(getIntent().getBundleExtra("username"));
+        String userName = String.valueOf(getIntent().getStringExtra("username"));
 
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null){
@@ -25,8 +46,101 @@ public class LeaderboardActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
-        Leaderboard leaderboard = new Leaderboard.LeaderboardBuilder(userName)
-                .setLinesUpperBound(10)
-                .build();
+
+        initializeLeaderboard(userName);
+        initializeSpinners();
+
+        final SwipeRefreshLayout pullToRefresh = findViewById(R.id.list_view_pull_to_refresh);
+        pullToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshLeaderboardView();
+                pullToRefresh.setRefreshing(false);
+            }
+        });
+
+
     }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+
+        if(leaderboard.getListOrderAsDescending()){
+            listFilter.setSelection(0);
+        }
+        else{
+            listFilter.setSelection(1);
+        }
+
+        listFilter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                leaderboard.setListOrderAsDescending(i == 0);
+                refreshLeaderboardView();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                ;
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        refreshLeaderboardView();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+    private void refreshLeaderboardView(){
+        leaderboard.refreshUserList();
+        usersDataList = leaderboard.getTopNDescendingUsers();
+        usersAdapter.notifyDataSetChanged();
+    }
+
+    /**
+     * Initialize the Leaderboard ListView and associated components.
+     * @param userName - to include in the dataset, if necessary.
+     */
+    private void initializeLeaderboard(String userName){
+        Leaderboard.LeaderboardBuilder builder = new Leaderboard.LeaderboardBuilder()
+                .setLinesUpperBound(10)
+                .initializeEntireUserList(true);
+        if(userName != null){builder.setUsername(userName);}
+
+        leaderboard = builder.build();
+
+        usersList = findViewById(R.id.leaderboard_content);
+        usersAdapter = new LeaderboardAdapter(this,usersDataList);
+        usersList.setAdapter(usersAdapter);
+    }
+
+    /**
+     * Initialize the Spinners at the top of the activity.
+     */
+    private void initializeSpinners(){
+        userScope = findViewById(R.id.spinner_user_scope);
+        listFilter = findViewById(R.id.spinner_filter);
+
+        String[] userScopeArray = {"Global","Nearby"};
+        String[] listFilterArray= {"Descending","Ascending"};
+        ArrayList<String> userScopeChoices = new ArrayList<>(Arrays.asList(userScopeArray));
+        ArrayList<String> listFilterChoices = new ArrayList<>(Arrays.asList(listFilterArray));
+        userScopeAdapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,userScopeChoices);
+        listFilterAdapter = new ArrayAdapter<>(this, com.google.android.material.R.layout.support_simple_spinner_dropdown_item,listFilterChoices);
+
+        userScope.setAdapter(userScopeAdapter);
+        listFilter.setAdapter(listFilterAdapter);
+    }
+
 }
