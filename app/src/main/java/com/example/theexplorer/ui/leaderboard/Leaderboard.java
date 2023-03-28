fdf99536d1a08083f422064958c2ff78ac267cf0
Andrew Rosario <71676291+andrew-rosario@users.noreply.github.com>
2023-03-27 18:05:38 -0600

    Add / Add user prioritization in Leaderboard ArrayList

--- app/src/main/java/com/example/theexplorer/ui/leaderboard/Leaderboard.java
+++ app/src/main/java/com/example/theexplorer/ui/leaderboard/Leaderboard.java
@@ -12,14 +12,18 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import org.checkerframework.checker.units.qual.A;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Leaderboard {
     final NewUserService userService = new NewUserService();
 
     private String currentUserID = null;
+    private User currentUserInstance;
+    private List<User> fullList;
+    private boolean isDescending = true;
     private int linesUpperBound = 10;
-    private List<User> fullList = null;
+
 
     /**
      * Initialize a Leaderboard with default settings.
@@ -32,7 +36,21 @@ public class Leaderboard {
      * Please see Builder constructor for more details.
      */
     private Leaderboard(LeaderboardBuilder builder){
-        if(this.currentUserID != null){ this.currentUserID = builder.currentUser;}
+        if(this.currentUserID != null){
+            this.currentUserID = builder.currentUser;
+            userService.getUser(builder.currentUser).addOnSuccessListener(new OnSuccessListener<User>() {
+                @Override
+                public void onSuccess(User user) {
+                    Log.d("TAG","User successfully found.");
+                    currentUserInstance = user;
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d("TAG","User not found or was not received.");
+                }
+            });
+        }
         if(builder.linesUpperBound != null){this.linesUpperBound = builder.linesUpperBound;}
         if(builder.preloadEntireList){refreshUserList();}
     }
@@ -59,6 +77,18 @@ public class Leaderboard {
         return ranking;
     }
 
+    public void setListOrderAsDescending(boolean toDescending){
+        boolean prior = this.isDescending;
+        this.isDescending = toDescending;
+        if(this.isDescending != prior){
+            Collections.reverse(fullList);
+        }
+    }
+    public boolean getListOrderAsDescending(){
+        return this.isDescending;
+    }
+
+
     /**
      * Refresh the list using the latest list from Firebase.
      */
@@ -75,16 +105,27 @@ public class Leaderboard {
                 Log.d("TAG", "Error getting list.");
             }
         });
+
+        if(!isDescending){
+            Collections.reverse(fullList);
+        }
     }
     public ArrayList<String> getTopNDescendingUsersAsStrings(){
         ArrayList<String> toReturn = new ArrayList<>();
         for(User user : getTopNDescendingUsers()){
             toReturn.add(user.getUserId());
         }
+        if (!toReturn.contains(currentUserID)){
+            toReturn.add(currentUserID); // this is problematic
+        }
         return toReturn;
     }
-    public ArrayList<User> getTopNDescendingUsers(){
-        return (ArrayList<User>) fullList.subList(0,linesUpperBound);
+    public ArrayList<User> getTopNDescendingUsers() {
+        ArrayList<User> truncatedList = new ArrayList<>(fullList.subList(0, linesUpperBound));
+        if(!truncatedList.contains(currentUserInstance)){
+            truncatedList.add(currentUserInstance);
+        }
+        return truncatedList;
     }
 
     //---------------------------------------------------------------------------------------------
@@ -96,14 +137,15 @@ public class Leaderboard {
 
         /**
          * Create a new Leaderboard object using the associated Builder.
-         * Focuses on the user; if user is beyond line n,
-         * the list will be n+1 long with the last being the user's ranking
-         * @param currentUser - the currentUser we want to focus
          */
-        public LeaderboardBuilder(String currentUser){
-            this.currentUser = currentUser;
+        public LeaderboardBuilder(){
         }
 
+
+        public LeaderboardBuilder setUsername(String username){
+            this.currentUser = username;
+            return this;
+        }
         /**
          * Modify the default lines upper bound.
          * @param upperBound - max lines to display
