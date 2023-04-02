8799d8d0e17709fa9fb4810d4fdaa1e15ba0635c
Gordon Chiang <gchiang56@gmail.com>
2023-04-01 22:49:00 -0600

    fix: ui refine activity_scores and implement getRankOfUser

--- app/src/main/java/com/example/theexplorer/ui/home/ScoresFragment.java
+++ app/src/main/java/com/example/theexplorer/ui/home/ScoresFragment.java
@@ -69,6 +69,16 @@ public class ScoresFragment extends AppCompatActivity {
 
                     TextView sumValue = findViewById(R.id.score_sum);
                     sumValue.setText(String.valueOf(getSumQRScores(arrayQRCode)));
+
+                    TextView playerRanking = findViewById(R.id.player_ranking);
+                    newUserService.getRankOfUser(user[0]).addOnSuccessListener(new OnSuccessListener<Integer>() {
+                        @Override
+                        public void onSuccess(Integer rank) {
+                            playerRanking.setText("Code Ranking: " + String.valueOf(rank));
+                            Log.d("RANK", String.valueOf(rank));
+                        }
+                    });
+
                 }
 
 
