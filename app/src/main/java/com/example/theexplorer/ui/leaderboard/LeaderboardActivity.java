6a14ac6bf0ff24514712fd88159f176900548207
Andrew Rosario <71676291+andrew-rosario@users.noreply.github.com>
2023-04-02 23:26:51 -0600

    Modify / Remove Descending/Ascending order filter.

--- app/src/main/java/com/example/theexplorer/ui/leaderboard/LeaderboardActivity.java
+++ app/src/main/java/com/example/theexplorer/ui/leaderboard/LeaderboardActivity.java
@@ -37,7 +37,7 @@ public class LeaderboardActivity extends AppCompatActivity {
     private ListView usersList;
 
     private final NewUserService userService = new NewUserService();
-    private boolean scoresDescending = true;
+    //private boolean scoresDescending = true;
     private boolean totalScoreMode = true;
     private int linesUpperBound = 50;
 
@@ -70,31 +70,31 @@ public class LeaderboardActivity extends AppCompatActivity {
             pullToRefresh.setRefreshing(false);
         });
 
-        listFilter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if(i == 1 && scoresDescending || i== 0 && !scoresDescending){
-                    scoresDescending = !scoresDescending;
-                    Collections.reverse(usersDataList);
-                    usersAdapter.notifyDataSetChanged();
-                    //refreshLeaderboardView();
-                }
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-                ;
-            }
-        });
+//        listFilter.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//                if(i == 1 && scoresDescending || i== 0 && !scoresDescending){
+//                    scoresDescending = !scoresDescending;
+//                    Collections.reverse(usersDataList);
+//                    usersAdapter.notifyDataSetChanged();
+//                    //refreshLeaderboardView();
+//                }
+//            }
+//            @Override
+//            public void onNothingSelected(AdapterView<?> adapterView) {
+//                ;
+//            }
+//        });
 
         scoreType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 if((i == 1 && totalScoreMode) || (i == 0 && !totalScoreMode)){
                     totalScoreMode = !totalScoreMode;
-                    listFilter.setSelection(0);
-                    if(!scoresDescending){
-                        scoresDescending = !scoresDescending;
-                    }
+                    //listFilter.setSelection(0);
+//                    if(!scoresDescending){
+//                        scoresDescending = !scoresDescending;
+//                    }
                     refreshLeaderboardView();
                 }
             }
@@ -164,13 +164,15 @@ public class LeaderboardActivity extends AppCompatActivity {
             }
             Collections.sort(usersDataList);
 
-            if(scoresDescending){i = 1;}
-            else{i = usersDataList.size();}
+//            if(scoresDescending){i = 1;}
+//            else{i = usersDataList.size();}
+            i = 1;
 
             for (RankingData data: usersDataList){
                 data.setRanking(i);
-                if(scoresDescending){i++;}
-                else{i--;}
+//                if(scoresDescending){i++;}
+//                else{i--;}
+                i++;
             }
 
             usersAdapter.notifyDataSetChanged();
@@ -196,17 +198,17 @@ public class LeaderboardActivity extends AppCompatActivity {
      */
     private void initializeSpinners(){
         scoreType = findViewById(R.id.spinner_score_type);
-        listFilter = findViewById(R.id.spinner_filter);
+        //listFilter = findViewById(R.id.spinner_filter);
 
         String[] userScopeArray = {"Total Score","Highest QR Score"};
-        String[] listFilterArray= {"Descending","Ascending"};
+        //String[] listFilterArray= {"Descending","Ascending"};
         ArrayList<String> scoreTypeChoices = new ArrayList<>(Arrays.asList(userScopeArray));
-        ArrayList<String> listFilterChoices = new ArrayList<>(Arrays.asList(listFilterArray));
+        //ArrayList<String> listFilterChoices = new ArrayList<>(Arrays.asList(listFilterArray));
         ArrayAdapter<String> scoreTypeAdapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, scoreTypeChoices);
-        ArrayAdapter<String> listFilterAdapter = new ArrayAdapter<>(this, com.google.android.material.R.layout.support_simple_spinner_dropdown_item, listFilterChoices);
+        //ArrayAdapter<String> listFilterAdapter = new ArrayAdapter<>(this, com.google.android.material.R.layout.support_simple_spinner_dropdown_item, listFilterChoices);
 
         scoreType.setAdapter(scoreTypeAdapter);
-        listFilter.setAdapter(listFilterAdapter);
+        //listFilter.setAdapter(listFilterAdapter);
     }
 
 }
